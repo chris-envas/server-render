f91d7aeb2567e544a795def10c333faec88d1abf
luojinxu <382967255@qq.com>
2019-06-24 18:20:08 +0800

    无ajax服务端渲染

---
+++ N-ajax/components/Bar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bar">
+    <h1>组件测试2</h1>
+    <img src="../src/assets/1.jpg" alt="壁纸">
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ N-ajax/components/Foo.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="foo">
+    <h1>组件测试1</h1>
+  </div>
+</template>
+
+<style>
+.foo {
+  background: yellowgreen;
+}
+</style>
